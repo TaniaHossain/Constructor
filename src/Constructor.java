ac6f7e988c63f5be27882cb29043a1cebdcfe496
Admin <Admin@Admin-PC>
2018-04-01 14:16:30 +0600

    init

--- src/Constructor.java
+++ src/Constructor.java
@@ -2,15 +2,24 @@
 public class Constructor {
 
    public static void main(String args[]) {
-      MyClass t1 = new MyClass();
+      System.out.println("Simple Constructor Calling Result:");
+	  MyClass t1 = new MyClass();
       MyClass t2 = new MyClass();
       System.out.println(t1.x + " " + t2.x);
+      System.out.println("Constructor with Parameter Calling Result:");
+      MyClass t3 = new MyClass( 13 );
+      MyClass t4 = new MyClass( 197 );
+      System.out.println(t3.x + " " + t4.x);
+      
    }
 }
 
 class MyClass {
 	   int x;
 	   MyClass() {
-	      x = 10;
+	      x = 77;
 	   }
+	   MyClass(int i ) {
+		      x = i;
+		   }
 	}
